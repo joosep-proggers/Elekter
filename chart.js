b793af66a7447d83029efae5587ef135812f54c0
joosep-proggers <joosep-elias.toru@khk.ee>
2021-12-16 19:06:08 +0200

    design complete, starting on back-end

--- chart.js
+++ chart.js
@@ -4,6 +4,7 @@ const ctx = document.getElementById('myChart').getContext('2d');
 ctx.canvas.width = window.innerWidth * 0.95;
 ctx.canvas.height = window.innerHeight * 0.5;
 
+// TODO: Kuidagi siia nordpool APIga andmed saada.
 let arr_backgroundColor = []
 let arr_data = [15.64,108.44,107.65,106.09,16.91,80.08,166.62,226.32,174.91,150.81,195.6,186.02,137.65,133.16,180.09,176.28,166.47,180.09,180.26,144.69,128.65,119.02,102.93,65.07]
 let avg_price = 135.39
@@ -25,17 +26,13 @@ console.log(arr_backgroundColor)
 
 const myChart = new Chart(ctx, {
 
-    type: 'line',
     label: 'Elektrihind Tunni Kohta',
-    // TODO: Kuidagi siia nordpool APIga andmed saada.
     data: avg_price,
-
-
-
     type: 'bar',
     color: '#999',
     data: {
         labels: ['00:00', '01:00', '02:00', '03:00', '04:00', '05:00','06:00', '07:00', '08:00', '09:00', '10:00','11:00', '12:00', '13:00', '14:00', '15:00', '16:00','17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00'],
+        fontColor: 'red',
         fontColor: 'rgba(66, 245, 120, 0.8)',
         fontSize: 20,
         datasets: [{
